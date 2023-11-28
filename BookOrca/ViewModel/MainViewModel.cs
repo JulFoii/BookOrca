bdb60fb7a79990fac981bb6ce80fa250385f29a4
Liiikely <Liiikely@gmx.de>
2023-11-28 09:46:44 +0100

    zurück
    Rückgängig des MaiNViewModels

--- BookOrca/ViewModel/MainViewModel.cs
+++ BookOrca/ViewModel/MainViewModel.cs
@@ -6,14 +6,7 @@ namespace BookOrca.ViewModel;
 
 public class MainViewModel : ViewModelBase
 {
-	public ObservableCollection<BookViewModel> Books { get; set; }
-
 	public MainViewModel()
 	{
-		Books = new ObservableCollection<BookViewModel>();
-		Books.Add(new BookViewModel(new Book()
-		{
-			CoverPath = "books/metadata/images/Best Loser Wins.pdf.png"
-		}));
 	}
 }
