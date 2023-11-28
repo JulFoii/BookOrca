ba83f96a5e10c343c6870a978f0a8265e0fdb63d
Julfoi <jfoitzik@gmx.net>
2023-11-28 11:23:39 +0100

    Removed BookInformation.cs and moved Props to Interface IBookApi.cs

--- BookOrca.ApiAccess/IBookApi.cs
+++ BookOrca.ApiAccess/IBookApi.cs
@@ -7,3 +7,11 @@ public interface IBookApi
     Task<BookInformation> GetBookInformation(string bookTitle);
 }
 
+public class BookInformation
+{
+    public string Title { get; set; } = string.Empty;
+    public string[] Authors { get; set; }
+    public string ISBN { get; set; } = string.Empty;
+    public string CoverUrl { get; set; } = string.Empty;
+}
+
--- BookOrca.Models/BookInformation.cs
+++
@@ -1,9 +0,0 @@
-namespace BookOrca.Models;
-
-public class BookInformation
-{
-        public string Title { get; set; }
-        public string[] Authors { get; set; }
-        public string ISBN { get; set; }
-        public string CoverUrl { get; set; }
-}
