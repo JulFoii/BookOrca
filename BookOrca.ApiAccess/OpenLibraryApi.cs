8c9ff3699f49b1748b4b2c253ad02c05a511f200
Mirco19 <95363954+Mirco19@users.noreply.github.com>
2023-11-28 11:09:15 +0100

    Created APi

--- BookOrca.ApiAccess/OpenLibraryApi.cs
+++ BookOrca.ApiAccess/OpenLibraryApi.cs
@@ -1,47 +1,54 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-public class TestApi
+public class OpenLibraryService : IBookApi
 {
-    private static async Task GetBookInformationFromOpenLibrary(string bookTitle)
+    public async Task<BookInformation> GetBookInformation(string bookTitle)
     {
-        var apiUrl = $"https://openlibrary.org/search.json?title={Uri.EscapeDataString(bookTitle)}";
+        string apiUrl = $"https://openlibrary.org/search.json?title={Uri.EscapeDataString(bookTitle)}";
 
         try
         {
-            using (var client = new HttpClient())
+            using (HttpClient client = new HttpClient())
             {
-                var response = await client.GetAsync(apiUrl);
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
                 response.EnsureSuccessStatusCode();
 
-                var responseBody = await response.Content.ReadAsStringAsync();
-
-                // Parsing des JSON-Antwortkörpers
+                string responseBody = await response.Content.ReadAsStringAsync();
                 dynamic data = JsonConvert.DeserializeObject(responseBody);
 
-                // Prüfen, ob Daten vorhanden sind und Ausgabe der Informationen für das erste Buch
                 if (data != null && data.docs != null && data.docs.Count > 0)
                 {
-                    var book = data.docs[0];
+                    dynamic book = data.docs[0];
                     string title = book.title;
                     string[] authors = book.author_name.ToObject<string[]>();
-                    string isbn = book.isbn != null && book.isbn.Count > 0 ? book.isbn[0] : "ISBN nicht verfügbar";
-                    var coverId = (int)book.cover_i;
-                    var coverUrl = $"https://covers.openlibrary.org/b/id/{coverId}-M.jpg";
+                    string isbn = (book.isbn != null && book.isbn.Count > 0) ? book.isbn[0] : "ISBN nicht verfügbar";
+                    int coverId = (int)book.cover_i;
+                    string coverUrl = $"https://covers.openlibrary.org/b/id/{coverId}-M.jpg";
 
-                    Console.WriteLine("Titel: " + title);
-                    Console.WriteLine("Autor(en): " + string.Join(", ", authors));
-                    Console.WriteLine("ISBN: " + isbn);
-                    Console.WriteLine("Cover URL: " + coverUrl);
+                    return new BookInformation
+                    {
+                        Title = title,
+                        Authors = authors,
+                        ISBN = isbn,
+                        CoverUrl = coverUrl
+                    };
                 }
                 else
                 {
-                    Console.WriteLine("Keine Informationen gefunden für " + bookTitle);
+                    // Keine Informationen gefunden
+                    return null;
                 }
             }
         }
         catch (HttpRequestException e)
         {
+            // Fehler beim Abrufen der Daten
             Console.WriteLine("Fehler beim Abrufen der Buchinformationen: " + e.Message);
+            return null;
         }
     }
 }
