5079538282ab6ad2fd369a2d2ce3c04182417d3c
Julfoi <jfoitzik@gmx.net>
2023-11-28 11:51:15 +0100

    Deleted unused Usings

--- BookOrca/ViewModel/BookViewModel.cs
+++ BookOrca/ViewModel/BookViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using BookOrca.Core;
