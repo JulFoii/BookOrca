f5715a94bdb2a6316b21ce6da90f62c0cac924d5
Julfoi <jfoitzik@gmx.net>
2023-11-28 11:15:01 +0100

    Added namespace for OpenLibraryApi.cs

--- BookOrca.ApiAccess/OpenLibraryApi.cs
+++ BookOrca.ApiAccess/OpenLibraryApi.cs
@@ -1,8 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+
+namespace BookOrca.ApiAccess;
 
 public class OpenLibraryService : IBookApi
 {
