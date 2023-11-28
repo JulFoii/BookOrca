a3574916e648391dc99eda97c2c3109c873c992f
Mirco19 <95363954+Mirco19@users.noreply.github.com>
2023-11-28 09:50:04 +0100

    Added APi

--- BookOrca.ApiAccess/IBookApi.cs
+++ BookOrca.ApiAccess/IBookApi.cs
@@ -1,5 +1,15 @@
-﻿namespace BookOrca.ApiAccess;
+﻿using System.Threading.Tasks;
+namespace BookOrca.ApiAccess;
 
 public interface IBookApi
 {
+    Task<BookInformation> GetBookInformation(string bookTitle);
+}
+
+public class BookInformation
+{
+    public string Title { get; set; }
+    public string[] Authors { get; set; }
+    public string ISBN { get; set; }
+    public string CoverUrl { get; set; }
 }
