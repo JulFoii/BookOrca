56240812f49573fc169b997ce31cdc93b9f8d45d
Liiikely <Liiikely@gmx.de>
2023-11-28 09:43:51 +0100

    MainWindow View und BookViewModel
    MainWindow Erstellung der DataContexte und implementierung des DataTemplates für die Bücher aus dem BookViewModel
    
    dazu wurde ein Grid Erstellt wo die Bücher dargestellt werden sollen.
    
    Eine Klasse erstellt, dass soll das ViewModel für die Bücher sein

--- BookOrca/ViewModel/MainViewModel.cs
+++ BookOrca/ViewModel/MainViewModel.cs
@@ -1,7 +1,19 @@
-﻿using BookOrca.Core;
+﻿using System.Collections.ObjectModel;
+using BookOrca.Core;
+using BookOrca.Models;
 
 namespace BookOrca.ViewModel;
 
 public class MainViewModel : ViewModelBase
 {
+	public ObservableCollection<BookViewModel> Books { get; set; }
+
+	public MainViewModel()
+	{
+		Books = new ObservableCollection<BookViewModel>();
+		Books.Add(new BookViewModel(new Book()
+		{
+			CoverPath = "books/metadata/images/Best Loser Wins.pdf.png"
+		}));
+	}
 }
