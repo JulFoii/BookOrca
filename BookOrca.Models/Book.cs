7100f8ac1645f37f0aac44ab02584e8ecce178c4
Julfoi <jfoitzik@gmx.net>
2023-11-28 08:27:31 +0100

    Added Properties for Book

--- BookOrca.Models/Book.cs
+++ BookOrca.Models/Book.cs
@@ -2,4 +2,18 @@
 
 public class Book
 {
+    public string Titel { get; set; } = string.Empty;
+
+    public string Autor { get; set; } = string.Empty;
+
+    public string CoverPath { get; set; } = string.Empty;
+
+    public string Path { get; set; } = string.Empty;
+
+    public string Isbn { get; set; } = string.Empty;
+    
+    private Book()
+    {
+        
+    }
 }
