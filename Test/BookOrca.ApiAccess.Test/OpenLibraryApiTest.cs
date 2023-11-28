b6a3048a0e7b62c849eb393e7f181ade354cf325
Mirco19 <95363954+Mirco19@users.noreply.github.com>
2023-11-28 12:24:03 +0100

    Added OpenLibraryUnitTest

---
+++ Test/BookOrca.ApiAccess.Test/OpenLibraryApiTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookOrca.ApiAccess.Test
+{
+    [TestFixture]
+    public class OpenLibraryServiceTests
+    {
+        public OpenLibraryService _openLibraryService;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initialisierung des Service
+            _openLibraryService = new OpenLibraryService();
+        }
+
+        [Test]
+        public async Task GetBookInformation_ValidTitle_ReturnsBookInfo()
+        {
+            // Arrange
+            string validBookTitle = "Harry Potter"; // Gültiger Buchtitel
+
+            // Act
+            BookInformation bookInfo = await _openLibraryService.GetBookInformation(validBookTitle);
+
+            // Assert
+            Console.WriteLine(bookInfo.Title + bookInfo.Authors + bookInfo.ISBN + bookInfo.CoverUrl);
+            Assert.IsNotNull(bookInfo);
+            Assert.IsNotNull(bookInfo.Title);
+            Assert.IsNotNull(bookInfo.Authors[0]);
+            Assert.IsNotNull(bookInfo.ISBN);
+            Assert.IsNotNull(bookInfo.CoverUrl);
+        }
+    }
+}
