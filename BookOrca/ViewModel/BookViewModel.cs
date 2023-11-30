6f3dc5017efba1c019cbd0862d46328b2979faed
Moritz Niesen <niesenmoritz@gmail.com>
2023-11-30 09:17:43 +0100

    Added new features to the BookViewModel

--- BookOrca/ViewModel/BookViewModel.cs
+++ BookOrca/ViewModel/BookViewModel.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Diagnostics;
+using System.Linq;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using BookOrca.Core;
+using BookOrca.DataAccess;
 using BookOrca.Models;
+using BookOrca.Resources;
 
 namespace BookOrca.ViewModel;
 
@@ -13,25 +14,37 @@ public class BookViewModel : ViewModelBase
     public BookViewModel(Book book)
     {
         Book = book;
-        CoverSource =
-            new BitmapImage(new Uri(
-                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "books/metadata/images/Best Loser Wins.pdf.png"),
-                UriKind.Absolute));
+        CoverSource = new BitmapImage(Paths.GetAbsoluteUri(Paths.GetImagePath(book.FileName)));
 
-        OpenFolerCommand = new RelayCommand(OpenFolder);
         DeleteBookCommand = new RelayCommand(DeleteBook);
+        OpenBookCommand = new RelayCommand(OpenBook);
+    }
+
+    #region Commands
+
+    public RelayCommand OpenBookCommand { get; }
+    public RelayCommand DeleteBookCommand { get; }
+
+    #endregion
+
+    #region Command Implementation
+
+    private void OpenBook()
+    {
+        Process.Start(new ProcessStartInfo(Paths.GetAbsoluteBookPath(Book.FileName))
+        {
+            UseShellExecute = true
+        });
     }
 
     private void DeleteBook()
     {
+        IBookDataAccess.Instance.DeleteBook(Book);
         MainViewModel.Instance.BookList.Remove(this);
     }
 
-    private void OpenFolder(object? parameter)
-    {
-        if (parameter is Book buch) Process.Start("explorer.exe", buch.FileName);
-    }
-    
+    #endregion
+
     #region Properties
 
     public ImageSource CoverSource { get; set; }
@@ -39,9 +52,21 @@ public class BookViewModel : ViewModelBase
 
     #endregion
 
-    #region Commands
-    public RelayCommand OpenFolerCommand { get; }
-    public RelayCommand DeleteBookCommand { get; }
+    #region Operator
+
+    public static implicit operator Book(BookViewModel bookViewModel)
+    {
+        return bookViewModel.Book;
+    }
+
+    public static implicit operator BookViewModel(Book book)
+    {
+        var bookViewModel = MainViewModel.Instance.BookList
+                                .FirstOrDefault(x => x.Book == book)
+                            ?? new BookViewModel(book);
+
+        return bookViewModel;
+    }
 
     #endregion
 }
