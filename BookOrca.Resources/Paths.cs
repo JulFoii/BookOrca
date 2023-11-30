6efc49b68ae4b59ccafd52f5369f80d55982ac15
Moritz Niesen <niesenmoritz@gmail.com>
2023-11-30 07:28:33 +0100

    Created Project Folder for Tests

---
+++ BookOrca.Resources/Paths.cs
@@ -0,0 +1,18 @@
+﻿namespace BookOrca.Resources;
+
+public static class Paths
+{
+    public const string RelativeImagesPath = "books/images";
+    public const string RelativeMetadataPath = "books/metadata";
+    public const string RelativeBookPath = "books";
+
+    public static string GetRelativeImagePath(string bookPath)
+    {
+        return "";
+    }
+
+    public static string RelativeToAbsolutePath(string relativePath)
+    {
+        return "";
+    }
+}
